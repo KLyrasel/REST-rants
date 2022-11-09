67e58fc985200c2b9cdfe8316844287f17f3d0d5
Klyrasel <KaiLyrasel@outlook.com>
2022-11-08 23:42:06 -0500

    added buttons

--- controllers/places.js
+++ controllers/places.js
@@ -38,5 +38,31 @@ router.post('/', (req, res) => {
   res.redirect('/places')
 })
 
-      
+router.get('/:id/edit', (req, res) => {
+  let id = Number(req.params.id)
+  if (isNaN(id)) {
+    res.render('error404')
+  }
+  else if (!places[id]) {
+    res.render('error404')
+  }
+  else {
+    res.render('places/edit', { place: places[id] , id: id})
+  }
+})   
+
+router.delete('/:id', (req, res) => {
+  let id = Number(req.params.id)
+  if (isNaN(id)) {
+    res.render('error404')
+  }
+  else if (!places[id]) {
+    res.render('error404')
+  }
+  else {
+    places.splice(id, 1)
+    res.send('STUB DELETE places/:id')
+  }
+})
+
 module.exports = router
--- views/places/show.jsx
+++ views/places/show.jsx
@@ -9,12 +9,19 @@ function show () {
             <h2>Rating</h2>
             <p>Not Rated</p>
             <h2>Description</h2>
-            <p>In Progres</p>
+            <p>In Progress</p>
             <div>
                 <h2>Comments</h2>
                 <p>No comments yet!</p>
             </div>
-        
+            <a href={'/places/${data.id}/edit'} className="btn btn-warning"> 
+                Edit
+            </a>  
+            <form method="POST" action={'/places/${data.id}?_method=DELETE'}> 
+            <button type="submit" className="btn btn-danger">
+            Delete
+            </button>
+            </form>
           </main>
         </Def>
     )
