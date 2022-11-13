64e27314dab35d7ac3270000a743a56a8257c379
Klyrasel <KaiLyrasel@outlook.com>
2022-11-13 00:15:57 -0500

    Finished app

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -7,7 +7,25 @@ function show (data) {
       No comments yet!
     </h3>
   )
+  let rating = (
+    <h3 className="inactive">
+      Not yet rated
+    </h3>
+  )
   if (data.place.comments.length) {
+    let sumRatings = data.place.comments.reduce((tot,c) => {
+      return tot + c.stars
+    },0)
+    let averageRating = Math.round(sumRatings / data.place.comments.length)
+    let stars = ''
+    for (let i = 0; i < averageRating; i++) {
+      stars += '⭐'
+    }
+    rating = (
+      <h3>
+        {stars} stars
+      </h3>
+    )
     comments = data.place.comments.map(c => {
       return (
         <div className="border">
@@ -32,7 +50,7 @@ function show (data) {
             </h3>
             <h1>Show Page</h1>
             <h2>Rating</h2>
-            <p>Not Rated</p>
+            <p>{rating}</p>
             <div className="col-sm-6"/>
             <h2>Description</h2>
             <p>In Progress</p>
